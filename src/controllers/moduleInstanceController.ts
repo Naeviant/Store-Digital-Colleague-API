8fc40356afc54a98cc64b2d4457c4c3cda8b6997
Sam Hirst <45145746+Naeviant@users.noreply.github.com>
2020-12-28 21:15:42 +0000

    added endpoints to add/remove modules to bays

--- src/controllers/moduleInstanceController.ts
+++ src/controllers/moduleInstanceController.ts
@@ -36,20 +36,50 @@ export const addModuleToSite = async (req: Request, res: Response): Promise<void
 	}
 };
 
+export const getModuleAtSite = async (req: Request, res: Response): Promise<void> => {
+	try {
+		if (!req.params.code || !req.params.discriminator) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Get Module at Site: Invalid Site Code or Discriminator')); 
+		else axios.get(`${config.base}/site/${req.params.code}`).then((response: AxiosResponse) => {
+			const site = response.data.data;
+			axios.get(`${config.base}/module/${req.params.discriminator}`).then((response: AxiosResponse) => {
+				const module = response.data.data;
+				ModuleInstance.findOne({ module: module._id, site: site._id }, { __v: 0 })
+					.populate({ path: 'site', select: '-__v'})
+					.populate({ path: 'module', select: '-__v', populate: { path: 'products.product', model: 'Product', select: '-__v' }})
+					.populate({ path: 'bay', select: '-__v', populate: { path: 'aisle', select: '-__v', populate: { path: 'site', select: '-__v' } } })
+					.then((doc: IModuleInstance | null) => {
+						if (!doc) res.status(404).send(new ModuleInstanceResponse(404, 'Cannot Get Module at Site: Module Not at Site'));
+						else res.status(200).send(new ModuleInstanceResponse(200, 'Module at Site Retrieved Successfully', doc));
+					}, (error: Error & { code: number } | null) => {
+						if (error) generate500(req, res, error);
+					});
+			}).catch((error: Error & { response: { status: number } } | null) => {
+				if (error && error.response && (error.response.status === 404 || error.response.status === 400)) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Add Module to Site: Invalid Discriminator Provided'));
+				else if (error) generate500(req, res, error);
+			});
+		}).catch((error: Error & { response: { status: number } } | null) => {
+			if (error && error.response && (error.response.status === 404 || error.response.status === 400)) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Add Module to Site: Invalid Site Code Provided'));
+			else if (error) generate500(req, res, error);
+		});
+	} catch (error) {
+		generate500(req, res, error);
+	}
+};
+
 export const getModulesAtSite = async (req: Request, res: Response): Promise<void> => {
 	try {
 		if (!req.params.code) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Get Modules from Site: Invalid Site Code Provided')); 
-		else axios.get(`${config.base}/site/${req.body.code}`).then((response: AxiosResponse) => {
+		else axios.get(`${config.base}/site/${req.params.code}`).then((response: AxiosResponse) => {
 			const site = response.data.data;
 			ModuleInstance.find({ site: site._id }, { _id: 0, __v: 0 })
 				.populate({ path: 'site', select: '-_id -__v'})
 				.populate({ path: 'module', select: '-_id -__v -products._id', populate: { path: 'products.product', model: 'Product', select: '-_id -__v' }})
-				// TODO: POPULATE BAY
+				.populate({ path: 'bay', select: '-_id -__v', populate: { path: 'aisle', select: '-_id -__v', populate: { path: 'site', select: '-_id -__v' } } })
 				.then((docs: IModuleInstance[] | null) => {
-				res.status(200).send(new ModuleInstanceResponse(200, 'Modules at Site Retrieved Successfully', docs));
-			}, (error: Error & { code: number } | null) => {
-				if (error) generate500(req, res, error);
-			});
+					res.status(200).send(new ModuleInstanceResponse(200, 'Modules at Site Retrieved Successfully', docs));
+				}, (error: Error & { code: number } | null) => {
+					if (error) generate500(req, res, error);
+				});
 		}).catch((error: Error & { response: { status: number } } | null) => {
 			if (error && error.response && (error.response.status === 404 || error.response.status === 400)) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Get Modules from Site: Invalid Site Code Provided'));
 			else if (error) generate500(req, res, error);
@@ -67,7 +97,7 @@ export const deleteModuleFromSite = async (req: Request, res: Response): Promise
 			axios.get(`${config.base}/module/${req.params.discriminator}`).then((response: AxiosResponse) => {
 				const module = response.data.data;
 				ModuleInstance.deleteOne({ module: module._id, site: site._id }).then((doc: { deletedCount: number }) => {
-					if (doc.deletedCount === 0) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Delete Module from Site: Module Not at Site'));
+					if (doc.deletedCount === 0) res.status(404).send(new ModuleInstanceResponse(404, 'Cannot Delete Module from Site: Module Not at Site'));
 					else res.status(200).send(new ModuleInstanceResponse(200, 'Module Deleted from Site Successfully'));
 				}, (error: Error & { code: number } | null) => {
 					if (error) generate500(req, res, error);
@@ -80,6 +110,92 @@ export const deleteModuleFromSite = async (req: Request, res: Response): Promise
 			if (error && error.response && (error.response.status === 404 || error.response.status === 400)) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Delete Module from Site: Invalid Site Code Provided'));
 			else if (error) generate500(req, res, error);
 		});
+	} catch (error) {
+		generate500(req, res, error);
+	}
+};
+
+export const addModuleToBay = async (req: Request, res: Response): Promise<void> => {
+	try {
+		if (!req.params.code || !req.params.aisle || !req.params.bay) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Assign Module: Invalid Site Code, Aisle Number or Bay Number Provided'));
+		else if (!req.body.discriminator) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Assign Module: Invalid Discriminator'));
+		else axios.get(`${config.base}/bay/${req.params.code}/${req.params.aisle}/${req.params.bay}`).then((response: AxiosResponse) => {
+			const bay = response.data.data;
+			axios.get(`${config.base}/module/${req.body.discriminator}`).then((response: AxiosResponse) => {
+				const module = response.data.data;
+				axios.get(`${config.base}/module/site/${req.params.code}/${req.params.aisle}/${req.params.bay}`).then((response: AxiosResponse) => {
+					const bayModules = response.data.data;
+					if (bayModules.length >= bay.moduleLimit) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Assign Module: Bay is Full'));
+					else ModuleInstance.updateOne({ site: bay.aisle.site._id, module: module._id }, { '$set': { bay: bay._id } }).then((docs: { n: number, nModified: number }) => {
+						if (docs.n === 0) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Assign Module: Invalid Site Code, Aisle Number, Bay Number or Discriminator Provided'));
+						else res.status(200).send(new ModuleInstanceResponse(200, 'Module Assigned Successfully'));
+					}, (error: Error & { code: number } | null) => {
+						if (error) generate500(req, res, error);
+					});
+				}).catch((error: Error & { response: { status: number } } | null) => {
+					if (error && error.response && (error.response.status === 404 || error.response.status === 400)) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Assign Module: Invalid Site Code, Aisle Number or Bay Number Provided'));
+					else if (error) generate500(req, res, error);
+				});
+			}).catch((error: Error & { response: { status: number } } | null) => {
+				if (error && error.response && (error.response.status === 404 || error.response.status === 400)) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Assign Module: Invalid Discriminator Provided'));
+				else if (error) generate500(req, res, error);
+			}); 
+		}).catch((error: Error & { response: { status: number } } | null) => {
+			if (error && error.response && (error.response.status === 404 || error.response.status === 400)) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Assign Module: Invalid Site Code, Aisle Number or Bay Number Provided'));
+			else if (error) generate500(req, res, error);
+		}); 
+	} catch (error) {
+		generate500(req, res, error);
+	}
+};
+
+export const getModulesInBay = async (req: Request, res: Response): Promise<void> => {
+	try {
+		if (!req.params.code || !req.params.aisle || !req.params.bay) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Get Modules in Bay: Invalid Site Code, Aisle Number or Bay Number Provided'));
+		else axios.get(`${config.base}/bay/${req.params.code}/${req.params.aisle}/${req.params.bay}`).then((response: AxiosResponse) => {
+			const bay = response.data.data;
+			ModuleInstance.find({ bay: bay._id }, { _id: 0, __v: 0 })
+				.populate({ path: 'site', select: '-_id -__v'})
+				.populate({ path: 'module', select: '-_id -__v -products._id', populate: { path: 'products.product', model: 'Product', select: '-_id -__v' }})
+				.populate({ path: 'bay', select: '-_id -__v', populate: { path: 'aisle', select: '-_id -__v', populate: { path: 'site', select: '-_id -__v' } } })
+				.then((docs: IModuleInstance[] | null) => {
+					res.status(200).send(new ModuleInstanceResponse(200, 'Modules in Bay Retrieved Successfully', docs));
+				}, (error: Error & { code: number } | null) => {
+					if (error) generate500(req, res, error);
+				});
+		}).catch((error: Error & { response: { status: number } } | null) => {
+			if (error && error.response && (error.response.status === 404 || error.response.status === 400)) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Assign Module: Invalid Site Code, Aisle Number or Bay Number Provided'));
+			else if (error) generate500(req, res, error);
+		}); 
+	} catch (error) {
+		generate500(req, res, error);
+	}
+};
+
+
+export const deleteModuleFromBay = async (req: Request, res: Response): Promise<void> => {
+	try {
+		if (!req.params.code || !req.params.aisle || !req.params.bay) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Unassign Module: Invalid Site Code, Aisle Number or Bay Number Provided'));
+		else if (!req.params.discriminator) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Unassign Module: Invalid Discriminator'));
+		else axios.get(`${config.base}/site/${req.params.code}`).then((response: AxiosResponse) => {
+			const site = response.data.data;
+			axios.get(`${config.base}/module/${req.params.discriminator}`).then((response: AxiosResponse) => {
+				const module = response.data.data;
+				ModuleInstance.updateOne({ site: site._id, module: module._id }, { '$set': { bay: null } }).then((docs: { n: number, nModified: number }) => {
+					if (docs.n === 0) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Unassign Module: Invalid Site Code or Discriminator Provided'));
+					else if (docs.nModified === 0) res.status(404).send(new ModuleInstanceResponse(404, 'Cannot Unassign Module: Module Not Assigned to Bay'));
+					else res.status(200).send(new ModuleInstanceResponse(200, 'Module Unassigned Successfully'));
+				}, (error: Error & { code: number } | null) => {
+					if (error) generate500(req, res, error);
+				});
+			}).catch((error: Error & { response: { status: number } } | null) => {
+				if (error && error.response && (error.response.status === 404 || error.response.status === 400)) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Unassign Module: Invalid Discriminator Provided'));
+				else if (error) generate500(req, res, error);
+			}); 
+		}).catch((error: Error & { response: { status: number } } | null) => {
+			if (error && error.response && (error.response.status === 404 || error.response.status === 400)) res.status(400).send(new ModuleInstanceResponse(400, 'Cannot Unassign Module: Invalid Site Code '));
+			else if (error) generate500(req, res, error);
+		}); 
 	} catch (error) {
 		generate500(req, res, error);
 	}
