ae3af9df9be5a2adcf57d809592eed66b99d9e6a
Sam Hirst <45145746+Naeviant@users.noreply.github.com>
2020-12-29 11:41:45 +0000

    added product quantity endpoints

--- src/entities/Site.ts
+++ src/entities/Site.ts
@@ -1,4 +1,6 @@
 import { Document, Schema, model } from 'mongoose';
+import { Product } from './Product';
+import { ProductQuantity } from './ProductQuantity';
 
 export interface ISite extends Document {
 	name: string;
@@ -8,6 +10,19 @@ export interface ISite extends Document {
 const siteSchema = new Schema({
 	name: { type: String, required: true },
 	code: { type: Number, required: true, unique: true }
+});
+
+siteSchema.post('save', (doc) => {
+	Product.find({}, async (err, products) => {
+		products.forEach(async product => {
+			const newProductQuantity = new ProductQuantity({
+				product: product._id,
+				site: doc._id,
+				quantity: 0
+			});
+			await newProductQuantity.save();
+		});
+	});
 });
 
 export const Site = model<ISite>('Site', siteSchema);
