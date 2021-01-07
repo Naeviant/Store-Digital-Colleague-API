b0f14e44afae7a0efe0f8d23269b43d340095998
Sam Hirst <45145746+Naeviant@users.noreply.github.com>
2021-01-07 20:41:27 +0000

    fixed linting errors

--- src/helpers/init.ts
+++ src/helpers/init.ts
@@ -3,28 +3,25 @@ import { Site } from '../entities/Site';
 import { User } from '../entities/User';
 
 export const init = async (): Promise<void> => {
-	return new Promise<void>(async (resolve, reject) => {
-		const users = await User.countDocuments({});
-		const sites = await Site.countDocuments({});
-		if (users === 0 && sites === 0) {
-			const newSite = new Site({
-				code: 1000,
-				name: 'First Store',
-				type: 'Store'
+	const users = await User.countDocuments({});
+	const sites = await Site.countDocuments({});
+	if (users === 0 && sites === 0) {
+		const newSite = new Site({
+			code: 1000,
+			name: 'First Store',
+			type: 'Store'
+		});
+		await newSite.save().then(async (site) => {
+			const password = await hash('password', 10);
+			const newUser = new User({
+				firstName: 'Default',
+				lastName: 'User',
+				username: 'admin',
+				password: password,
+				userType: 'Admin',
+				site: site._id
 			});
-			await newSite.save().then(async (site) => {
-				const password = await hash('password', 10);
-				const newUser = new User({
-					firstName: 'Default',
-					lastName: 'User',
-					username: 'admin',
-					password: password,
-					userType: 'Admin',
-					site: site._id
-				});
-				await newUser.save();
-			});
-			
-		}
-	});
-}
+			await newUser.save();
+		});	
+	}
+};
