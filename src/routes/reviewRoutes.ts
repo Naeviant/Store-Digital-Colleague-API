870a3d2c06c4c920639cbe27f5093b44b1fe88d1
Sam Hirst <45145746+Naeviant@users.noreply.github.com>
2021-01-21 20:21:59 +0000

    added review routes and controller

---
+++ src/routes/reviewRoutes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import * as reviewController from '../controllers/reviewController';
+import { isAdmin } from '../middleware/auth';
+import { getCustomer } from '../middleware/getCustomer';
+import { getProduct } from '../middleware/getProduct';
+import { generate405 } from '../helpers/respond';
+
+const router = Router();
+
+router.route('/review')
+.post(getCustomer, getProduct, reviewController.addReview)
+.all(generate405);
+router.route('/review/product/:ean')
+.get(getProduct, reviewController.getProductReviews)
+.all(generate405);
+router.route('/review/customer/:customer')
+.get(getCustomer, reviewController.getCustomerReviews)
+.all(generate405);
+router.route('/review/:customer/:ean')
+.delete(isAdmin, getCustomer, getProduct, reviewController.deleteReviews)
+.all(generate405);
+
+export const reviewRoutes = router;
